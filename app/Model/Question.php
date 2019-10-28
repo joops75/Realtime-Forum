7d31dd76c0e13757d6a9a0208aeac13d0f93d3c3
Simon Cooper <kidney9@hotmail.com>
2019-10-28 17:36:37 +0000

    Relationships added

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,20 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+// don't need to state Reply and Category models here as they are in the same folder as this file
 
 class Question extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function replies() {
+        return $this->hasMany(Reply::class);
+    }
+    
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -3,8 +3,19 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Reply extends Model
 {
-    //
+    public function user() {
+        $this->belongsTo(User::class);
+    }
+    
+    public function question() {
+        $this->belongsTo(Question::class);
+    }
+    
+    public function like() {
+        $this->hasMany(Like::class);
+    }
 }
