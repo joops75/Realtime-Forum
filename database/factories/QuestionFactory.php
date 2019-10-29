cb39340d0bef2edde2026596a9082aef5bea80b4
Simon Cooper <kidney9@hotmail.com>
2019-10-29 11:11:07 +0000

    Factories created

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -3,10 +3,23 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Question;
+use App\User;
+use App\Model\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence(5);
+
     return [
-        //
+        'title' => $title,
+        'slug' => Str::slug($title),
+        'body' => $faker->text(200),
+        'user_id' => function() {
+            return User::all()->random();
+        },
+        'category_id' => function() {
+            return Category::all()->random();
+        }
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -3,10 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Reply;
+use App\User;
+use App\Model\Question;
 use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text(200),
+        'user_id' => function() {
+            return User::all()->random();
+        },
+        'question_id' => function() {
+            return Question::all()->random();
+        }
     ];
 });
