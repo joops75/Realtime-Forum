5eb397dab1c2f27e8a1eee73daaa282dc4a9ad73
Simon Cooper <kidney9@hotmail.com>
2019-10-29 15:55:06 +0000

    Question CRUD and resource transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -8,6 +8,18 @@ use App\User;
 
 class Question extends Model
 {
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $guarded = [];
+
     public function user() {
         return $this->belongsTo(User::class);
     }
@@ -18,5 +30,11 @@ class Question extends Model
     
     public function category() {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute() {
+        // function accessed via $this->path
+        // asset function appends base url to passed in string
+        return asset("api/question/$this->slug");
     }
 }
