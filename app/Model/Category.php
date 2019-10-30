21bdddb2b0c988caa0791be6445539511af195f4
Simon Cooper <kidney9@hotmail.com>
2019-10-30 14:01:40 +0000

    CRUD for Category

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $guarded = [];
 }
