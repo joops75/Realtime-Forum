0d5955abf8532343f55c4eed98a6c2209565e8ac
Simon Cooper <kidney9@hotmail.com>
2019-10-30 14:01:20 +0000

    Update function added

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -52,7 +52,8 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('updated', Response::HTTP_ACCEPTED);
     }
 
     /**
